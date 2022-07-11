048a4072053c46db443ef7ca91802e03c662f8ad
Fiorella <fiorella.valdivia@hotmail.com>
2022-07-10 21:57:18 -0500

    actualizacion fiorella

--- proyecto-pw/src/App.js
+++ proyecto-pw/src/App.js
@@ -14,6 +14,11 @@ import Pagina_BAND from './pages/Pagina_BAND';
 import Pagina_MPRIV from './pages/Pagina_MPRIV';
 import Pagina_MPUB from './pages/Pagina_MPUB';
 import Pagina_RCM from './pages/Pagina_RCM';
+import Pagina_CP from "./pages/Pagina_CP";
+import Pagina_EP from "./pages/Pagina_EP";
+import Pagina_MP from "./pages/Pagina_MP";
+import Pagina_TP from "./pages/Pagina_TP";
+
 //import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -29,6 +34,11 @@ function App() {
     //<Pagina_BAND/>
     //<Pagina_MPRIV/>
     //<Pagina_MPUB/>
+    //<Pagina_EP/>
+    //<Pagina_CP/>
+    //<Pagina_MP/>
+    //<Pagina_TP/>
+    
     <Pagina_RCM/>
   );
 }
